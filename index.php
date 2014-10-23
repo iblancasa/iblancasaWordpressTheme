2162793186027cd5b5e13bb88e7d7c7eeb914dd0
Israel Blancas <iblancasa@gmail.com>
2014-10-23 14:51:47 +0200

    Cambiado el color de los enlaces en los post. Cambiado el color de los títulos H2 en los post. Cambiada la etiqueta de la fecha. Pasa de un H2 a un DIV Se elimina la fuente de los post. Ahora se ve más claro. Añadido un margen entre las listas. Añadido un margen entre los elementos de las listas. Disminuida la sangría.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
                      <?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
                     <article class="post">
                         <div class="tituloPost"><h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1></div>
-                        <h2 class="fecha"><?php the_time('j/m/Y') ?> por <?php the_author_posts_link() ?> </h2>
+                        <div class="fecha"><?php the_time('j/m/Y') ?> por <?php the_author_posts_link() ?> </div>
                         <?php the_content(); ?>
                     </article>
                       <?php endwhile; else: ?>
